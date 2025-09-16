73e427b6fe032ad92cd845cd2875441cbf1e5a54
Yash Singhal <152426138+mryashsinghal@users.noreply.github.com>
2025-09-16 20:50:34 +0530

    Update socket.js

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const BACKEND = import.meta.env.VITE_BACKEND || "http://localhost:4000";
+const BACKEND = import.meta.env.VITE_BACKEND || "https://focus-proctoring-backend.onrender.com";
 const socket = io(BACKEND, { autoConnect: true });
 
 export default socket;
